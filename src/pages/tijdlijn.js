032bb077abe4e09493b1e7dacb97cab48e374960
StanTeurlings <s_lings@hotmail.com>
2024-12-20 12:13:08 +0100

    fading sides for the timeline

--- src/pages/tijdlijn.js
+++ src/pages/tijdlijn.js
@@ -1,7 +1,9 @@
 export default function tijdlijn() {
     return (
-      <div>
-        <h2>Tijdlijn</h2>
+      <div className="scroll-container">
+        <img className="mt-5" src="/images/blue.png" alt="blue" width="200%" height="500px"/>
+        <div className="fade-effect fade-left ms-n1"></div>
+        <div className="fade-effect fade-right"></div>
       </div>
     );
   }

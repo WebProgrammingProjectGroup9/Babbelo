d961ed67d4ef7208833084f377e02c29e55db594
ECiftci1 <emreciftci2001@hotmail.com>
2025-01-09 14:16:28 +0100

    Profile page made

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,14 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import "@/styles/custom-bootstrap.scss";
 import "@/styles/globals.css";
 import Layout from '../components/Layout';
+import { AuthProvider } from '../components/AuthContext';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
   );
 }

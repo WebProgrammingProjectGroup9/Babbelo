1d117ccbf0dbbbffea56a5d8407e2618e1b26c14
Esmee-Stam <el.stam@student.avans.nl>
2025-01-21 10:17:18 +0100

    made timeline responsive

--- src/pages/tijdlijn.js
+++ src/pages/tijdlijn.js
@@ -1,226 +1,195 @@
-import React, { useState, useRef } from "react";
+import Link from "next/link";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Tijdlijn() {
-  const [events, setEvents] = useState([]);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const timelineRef = useRef(null);
-  const timelineLineRef = useRef(null);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
+    const [events, setEvents] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [visibleEvents, setVisibleEvents] = useState([]);
+    const timelineRef = useRef(null);
+    const [centerIndex, setCenterIndex] = useState(0);
+    const [adjacentIndex, setAdjacentIndex] = useState([-1, 1]);
+    const [activeEvent, setActiveEvent] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const debouncedHandleScroll = useRef(null);
 
-  const fetchEvents = async () => {
-    try {
-      const token = localStorage.getItem("token");
-      const userId = localStorage.getItem("account_id");
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/event/timeline/${userId}`, {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-        method: "GET",
-      });
+    const fetchEvents = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const userId = localStorage.getItem("account_id");
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/event/timeline/${userId}`, {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+                method: "GET",
+            });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-      const data = await response.json();
-      data.sort((a, b) => new Date(a.date) - new Date(b.date));
-      setEvents(data);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching events:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchEvents();
-  }, []);
-
-  const handleItemClick = (index) => {
-    setActiveIndex(index);
-  };
-  
-
-  const scrollTimeline = (index) => {
-    if (timelineRef.current && events.length > 0) {
-      const activeEvent = timelineRef.current.children[index];
-      if (activeEvent) {
-        // Bereken de breedte van de tijdlijn en het actieve item
-        const timelineWidth = timelineRef.current.offsetWidth;
-        const eventWidth = activeEvent.offsetWidth;
-  
-        // Bereken de scrollpositie voor gecentreerd scrollen
-        const scrollPosition =
-          activeEvent.offsetLeft - timelineWidth / 2 + eventWidth / 2;
-  
-        // Zorg ervoor dat de scrollpositie niet negatief is
-        const minScrollPosition = 0;
-        const finalScrollPosition = Math.max(scrollPosition, minScrollPosition);
-  
-        // Update de tijdlijn met een overgangseffect
-        timelineRef.current.style.transition = "transform 0.3s ease-out";
-        timelineRef.current.style.transform = `translateX(-${finalScrollPosition}px)`;
-      }
-    }
-  };
-  
-  useEffect(() => {
-    const handleScroll = () => {
-      if (timelineRef.current) {
-        const timelineItems = Array.from(timelineRef.current.children);
-        const scrollPosition = window.scrollY;
-  
-        // Zoek het dichtstbijzijnde item
-        const closestIndex = timelineItems.reduce(
-          (closest, item, index) => {
-            const rect = item.getBoundingClientRect();
-            const offsetTop = rect.top + window.scrollY;
-            const distance = Math.abs(scrollPosition - offsetTop);
-  
-            return distance < closest.distance
-              ? { distance, index }
-              : closest;
-          },
-          { distance: Infinity, index: 0 }
-        ).index;
-  
-        setActiveIndex(closestIndex);
-      }
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (timelineRef.current) {
-        const timelineItems = Array.from(timelineRef.current.children);
-        const scrollPosition = window.scrollY;
-  
-        const closestIndex = timelineItems.reduce((closest, item, index) => {
-          const rect = item.getBoundingClientRect();
-          const offsetTop = rect.top + window.scrollY;
-          const distance = Math.abs(scrollPosition - offsetTop);
-  
-          return distance < closest.distance
-            ? { distance, index }
-            : closest;
-        }, { distance: Infinity, index: 0 }).index;
-  
-        setActiveIndex(closestIndex);
-      }
+            const data = await response.json();
+            data.sort((a, b) => new Date(a.date) - new Date(b.date));
+            setEvents(data);
+            setLoading(false);
+        } catch (error) {
+            console.error("Fout bij het ophalen van evenementen:", error);
+        }
     };
-  
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-  
 
-  useEffect(() => {
-    if (events.length > 0) {
-      const now = new Date();
-      const closestEventIndex = events.findIndex((event) => new Date(event.date).setHours(0, 0, 0, 0) >= now.setHours(0, 0, 0, 0));
+    useEffect(() => {
+        fetchEvents();
+    }, []);
 
-      if (closestEventIndex === -1) {
-        setActiveIndex(events.length - 1);
-      } else {
-        setActiveIndex(closestEventIndex);
-      }
-    }
-  }, [events]);
+    const handleScroll = () => {
+        if (!timelineRef.current || loading) return;
 
-  useEffect(() => {
-    if (activeIndex !== null && events.length > 0) {
-      scrollTimeline(activeIndex);
-    }
-  }, [activeIndex, events]);
+        const timelineWidth = timelineRef.current.offsetWidth;
+        const timelineLeft = timelineRef.current.getBoundingClientRect().left;
+        let newVisibleEvents = [];
 
-  const scrollByDirection = (direction) => {
-    let newIndex = activeIndex + direction;
+        events.forEach((event, index) => {
+            const eventElement = document.getElementById(`event-${index}`);
+            if (eventElement) {
+                const eventLeft = eventElement.getBoundingClientRect().left;
+                const eventRight = eventLeft + eventElement.offsetWidth;
 
-    if (newIndex >= 0 && newIndex < events.length) {
-      setActiveIndex(newIndex);
-      scrollTimeline(newIndex);
-    }
-  };
+                if (eventRight >= timelineLeft && eventRight <= timelineLeft + timelineWidth) {
+                    newVisibleEvents.push(index);
+                }
+            }
+        });
 
-  const getTimelineDatePosition = (index) => {
-    return index % 2 === 0 ? { top: "20px" } : { bottom: "30px" };
-  };
+        if (newVisibleEvents.length !== visibleEvents.length || newVisibleEvents.some((value, index) => value !== visibleEvents[index])) {
+            setVisibleEvents(newVisibleEvents);
+            handleScale(newVisibleEvents);
+        }
+    };
 
-  return (
-    <div style={{ position: "relative", display: "flex", flexDirection: "column", height: "100vh" }}>
-      <div className="fade-right"></div>
-      <div className="fade-left"></div>
-  
-      <div className="timeline-container">
-        <div className="timeline-line" ref={timelineLineRef}></div>
-        <div className="timeline" ref={timelineRef}>
-          {events.map((event, index) => (
-            <div
-              key={index}
-              className={`timeline-item ${index === activeIndex ? "active" : ""}`}
-              onClick={() => handleItemClick(index)}
-            >
-              <div
-                className={`timeline-circle ${index === activeIndex ? "active" : ""}`}
-              ></div>
-  
-              <div className="timeline-date" style={{ position: "absolute", ...getTimelineDatePosition(index) }}>
-                {new Date(event.date).toLocaleDateString("nl-NL", {
-                  day: "numeric",
-                  month: "long",
-                  year: "numeric",
-                })}
-              </div>
-  
-              <div className="timeline-box">
-                {index === activeIndex && (
-                  <div className="participant-bubbles">
-                    {event.participants.slice(0, 5).map((participant, i) => (
-                      <Link href={`/account/${participant._id}`} key={i}>
-                        <div className="participant-container" style={{ position: "relative" }}>
-                          <img
-                            src={participant.profileImgUrl}
-                            alt={`${participant.firstName} ${participant.lastName}`}
-                          />
-                          <div className="participant-name">
-                            {participant.firstName} {participant.lastName}
-                          </div>
+    const debounce = (func, delay) => {
+        return (...args) => {
+            if (debouncedHandleScroll.current) {
+                clearTimeout(debouncedHandleScroll.current);
+            }
+            debouncedHandleScroll.current = setTimeout(() => {
+                func(...args);
+            }, delay);
+        };
+    };
+
+    const debouncedScrollHandler = debounce(handleScroll, 100);
+
+    useEffect(() => {
+        const scrollHandler = () => {
+            debouncedScrollHandler();
+        };
+
+        if (timelineRef.current) {
+            timelineRef.current.addEventListener("scroll", scrollHandler, { passive: true });
+        }
+
+        handleScroll();
+
+        return () => {
+            if (timelineRef.current) {
+                timelineRef.current.removeEventListener("scroll", scrollHandler);
+            }
+            clearTimeout(debouncedHandleScroll.current);
+        };
+    }, [events, visibleEvents, loading]);
+
+    const handleScale = (newVisibleEvents) => {
+        if (newVisibleEvents.length > 0) {
+            const centerEvent = newVisibleEvents[Math.floor(newVisibleEvents.length / 2)];
+            const adjacentEvents = [centerEvent + 1, centerEvent - 1];
+            setCenterIndex(centerEvent);
+            setAdjacentIndex(adjacentEvents);
+        } else {
+            setCenterIndex(null);
+            setAdjacentIndex([]);
+        }
+    };
+
+    const handleItemClick = (index) => {
+        if (index >= 0 && index < events.length) {
+            setActiveIndex(index);
+            setActiveEvent(events[index]);
+        }
+    };
+
+    const scrollTimeline = (direction) => {
+        if (timelineRef.current) {
+            const scrollAmount = 350;
+            const newScrollLeft = timelineRef.current.scrollLeft + direction * scrollAmount;
+            const maxScrollLeft = timelineRef.current.scrollWidth - timelineRef.current.clientWidth;
+
+            if (newScrollLeft >= 0 && newScrollLeft <= maxScrollLeft) {
+                timelineRef.current.scrollLeft = newScrollLeft;
+            }
+        }
+    };
+
+    const formatDate = (dateString) => {
+        const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        const date = new Date(dateString);
+        return date.toLocaleDateString('nl-NL', options);
+    };
+
+    return (
+		<div>
+            <div className="fade-right"></div>
+        <div style={{ position: "relative", overflow: "hidden", height: "100vh" }}>
+            <div className="fade-left"></div>
+            <div className="timeline-container">
+                <div className="timeline" ref={timelineRef}>
+                    <div className="timeline-spacer" style={{ width: "340px", flexShrink: 0 }}></div>
+                    <div className="timeline-line"></div>
+
+                    {events.map((event, index) => (
+                        <div
+                            id={`event-${index}`}
+                            key={index}
+                            className={`timeline-item ${index === activeIndex ? "active" : ""} ${index === centerIndex ? "center" : ""} ${adjacentIndex.includes(index) ? "adjacent" : ""}`}
+                            onClick={() => handleItemClick(index)}
+                        >
+                            <div className={`timeline-circle ${index === activeIndex ? "active" : ""}`}></div>
+                            <p>{formatDate(event.date)}</p>
+
+                            <div className="timeline-box">
+                                <img src={event.photo} alt={`Event ${index}`} className="timeline-image" />
+                                {index === activeIndex && (
+                                    <div>
+                                        <div>
+                                            <h4 className="display-6 event-title mt-1">{event.title}</h4>
+                                        </div>
+                                        <div className="participants-bubbles">
+                                            {event.participants.slice(0, 3).map((participant, idx) => (
+                                                <div key={idx} className="participant-bubble">
+                                                    <img src={participant.profileImgUrl} alt={`Participant ${idx}`} className="participant-image" />
+                                                </div>
+                                            ))}
+                                        </div>
+                                            <Link href={`/evenementen/${event.id}`}>
+                                                <button className="btn timeline-btn w-100">
+                                                    Lees meer
+                                                </button>
+                                            </Link>
+                                    </div>
+                                )}
+                            </div>
                         </div>
-                      </Link>
                     ))}
-                  </div>
-                )}
-                <img
-                  src={event.photo}
-                  alt={`Event ${index + 1}`}
-                  className="timeline-image"
-                />
-                {index === activeIndex && (
-                  <div className="timeline-details m-2">
-                    <div className="border-bottom">
-                      <h4 className="display-6">{event.title}</h4>
-                    </div>
-                   
-                    <button className="btn btn-primary w-100" onClick={() => router.push(`/evenementen/${event.id}`)}>Bekijken</button>
-                  </div>
-                )}
-              </div>
+                </div>
             </div>
-          ))}
+
+            <div className="scroll-arrows">
+                <button className="scroll-arrow left" onClick={() => scrollTimeline(-1)}>
+                    &#10094;
+                </button>
+                <button className="scroll-arrow right" onClick={() => scrollTimeline(1)}>
+                    &#10095;
+                </button>
+            </div>
         </div>
-      </div>
-  
-      <div className="scroll-arrows">
-        <button className="scroll-arrow left" onClick={() => scrollByDirection(-1)}>
-          &#10094;
-        </button>
-        <button className="scroll-arrow right" onClick={() => scrollByDirection(1)}>
-          &#10095;
-        </button>
-      </div>
-    </div>
-  );
-  
+		</div>
+    );
 }
